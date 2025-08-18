cc7a031dbc3292492d758be7e41ad5e0c6a1c420
Shekapuram lokesh chandra <144873300+lokeshshekapuram@users.noreply.github.com>
2025-08-18 22:00:01 +0530

    Update Stock.js

--- backend/models/Stock.js
+++ backend/models/Stock.js
@@ -4,7 +4,7 @@ const stockSchema = new mongoose.Schema({
     symbol: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     addedAt: { type: Date, default: Date.now },
-    isWatched: { type: Boolean, default: true }
+    isWatched: { type: Boolean, default: true } 
 });
 
 module.exports = mongoose.model('Stock', stockSchema);
